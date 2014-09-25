9fbd5bfaa9ce2779d7e5bc526d6f1627d1459f06
Wolf Vollprecht <w.vollprecht@gmail.com>
2014-09-25 13:58:40 +0200

    removed htaccess

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -157,7 +157,7 @@ class Page_Controller extends ContentController {
 
 	public function init() {
 		parent::init();
-		Requirements::javascript("https://code.jquery.com/jquery-2.1.1.min.js");
+		Requirements::javascript("//code.jquery.com/jquery-1.11.0.min.js");
 		Requirements::combine_files("ec.js", array(
 			"themes/EC/sass/foundation/js/foundation/foundation.js",
 			"themes/EC/sass/foundation/js/foundation/foundation.topbar.js",
