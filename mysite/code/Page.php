e3e36c3717f7e5900cbe9c1e1a371c6681547fe5
Wolf Vollprecht <w.vollprecht@gmail.com>
2014-09-25 07:42:35 +0200

    silverstripizing

---
+++ mysite/code/Page.php
@@ -0,0 +1,156 @@
+<?php
+
+
+class Advisor extends DataObject {
+	private static $default_sort = "SortOrder";
+	private static $db = array(
+		"Name" => "Text",
+		"Position" => "Text",
+		"SortOrder" => "Int",
+	);
+	private static $has_one = array(
+		"ParentPage" => "Page",
+		"AdvisorImage" => "Image"
+	);
+
+	public function getCMSFields() {
+		$uf = new UploadField("CardImage");
+		$uf->setFolderName('Advisors');
+		return new FieldList(
+			new TextField("Name"),
+			new TextField("Position"),
+			$uf
+		);
+		// return parent::getCMSFields();
+	}
+
+}
+
+
+class Card extends DataObject {
+	private static $default_sort = "SortOrder";
+
+	private static $db = array(
+		"Name" => "Text",
+		"URL" => "Text",
+		"BackgroundColor" => "Text",
+		"SortOrder" => "Int",
+		"SponsorType" => "Text"
+	);
+	private static $has_one = array(
+		"Sponsor" => "Page",
+		"Partner" => "Page",
+		"CardImage" => "Image"
+	);
+
+	public function getCMSFields() {
+		$uf = new UploadField("CardImage");
+		$uf->setFolderName('Cards');
+		return new FieldList(
+			new TextField("Name"),
+			new TextField("URL"),
+			new TextField("SponsoringType"),
+			new ColorField('BackgroundColor'),
+			$uf
+		);
+		// return parent::getCMSFields();
+	}
+}
+
+
+
+class Event extends DataObject {
+	private static $default_sort = "Date";
+	private static $db = array(
+		"Title" => "Text",
+		"Description" => "HTMLText",
+		"FacebookLink" => "Varchar(255)",
+		"Date" => "Date",
+		"EndDate" => "Date",
+		"Location" => "Varchar(255)"
+	);
+	private static $has_one = array(
+		"ParentPage" => "Page",
+		"Image" => "Image"
+	);
+
+	public function getCMSFields() {
+		DateField::set_default_config('showcalendar', true);
+		return new FieldList(
+			new TextField("Title"),
+			new TextField("FacebookLink"),
+			new UploadField("Image"),
+			new DateField("Date"),
+			new DateField("EndDate", "End Date if multiple days (otherwise leave empty)"),
+			new TextField("Location"),
+			new HTMLEditorField("Description")
+		);
+	}
+}
+
+class OnePagerPage extends Page {
+	private static $has_many = array(
+		'SponsorCards' => "Card.Sponsor",
+		'PartnerCards' => "Card.Partner",
+		'Events' => 'Event',
+		'Advisors' => 'Advisor'
+	);
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+	    $conf = GridFieldConfig_RelationEditor::create();
+	    $conf->addComponent(new GridFieldOrderableRows('SortOrder'));;
+		$gridField = new GridField('sponsorcards', 'Sponsor Cards', $this->SponsorCards(), $conf);
+
+		$fields->addFieldToTab('Root.SponsorsAndPartners', $gridField);
+		$gridField2 = new GridField('partnercards', 'Partner Cards', $this->PartnerCards(), $conf);
+		$fields->addFieldToTab('Root.SponsorsAndPartners', $gridField2);
+
+		$gridField3 = new GridField('events', 'Events', $this->Events(), GridFieldConfig_RelationEditor::create());
+		$fields->addFieldToTab('Root.Events', $gridField3);
+
+		$gridField4 = new GridField('advisors', 'Advisors', $this->Advisors(), $conf);
+		$fields->addFieldToTab('Root.Advisors', $gridField4);
+
+
+		return $fields;
+	}
+
+}
+
+class Page extends SiteTree {
+	private static $db = array(
+	);
+
+	private static $has_one = array(
+	);
+
+}
+class Page_Controller extends ContentController {
+
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+	private static $allowed_actions = array (
+	);
+
+	public function init() {
+		parent::init();
+		// You can include any CSS or JS required by your project here.
+		// See: http://doc.silverstripe.org/framework/en/reference/requirements
+	}
+
+}
